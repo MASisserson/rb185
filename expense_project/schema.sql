e49220d164d36d8b17b99787e7e272bbbd886ba8
Matthew Sisserson <masisserson@gmail.com>
2022-05-24 09:30:09 -0400

    Got further

--- expense_project/schema.sql
+++ expense_project/schema.sql
@@ -1,12 +1,9 @@
 CREATE TABLE expenses (
   id serial PRIMARY KEY,
-  amount numeric(6, 2) NOT NULL,
+  amount numeric(6, 2) NOT NULL CHECK (amount >= 0.01),
   memo text NOT NULL,
   created_on date NOT NULL
 );
-
-ALTER TABLE expenses
-  ADD CONSTRAINT positive_amount_check CHECK (amount >= 0.00);
 
 INSERT INTO expenses (amount, memo, created_on)
   VALUES (14.56, 'Pencils', NOW()),
